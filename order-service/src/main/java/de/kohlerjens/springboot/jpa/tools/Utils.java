d7ad87a1cd16a7f650919ea9be28c939f9c09938
Jens Kohler <vielleicht@web.de>
2022-03-30 17:41:00 +0200

    add order service

---
+++ order-service/src/main/java/de/kohlerjens/springboot/jpa/tools/Utils.java
@@ -0,0 +1,13 @@
+package de.kohlerjens.springboot.jpa.tools;
+
+import java.sql.Date;
+
+public class Utils {
+    public static boolean checkNullString(String stringToCheck) {
+        return stringToCheck != null;
+    }
+
+    public static boolean checkNullString(Date stringToCheck) {
+        return stringToCheck != null;
+    }
+}
